40ec7b9130e4c07d59c30d8a7fea49bfe4d32aa7
Oscar España <espaaoscar@gmail.com>
2020-11-29 00:06:13 -0500

    Crear nuevo héroe mediante la petición post

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { Card, Row, Col, Button } from "antd";
+import { Card, Row, Col, Button, Modal, Input } from "antd";
 import { DeleteOutlined, EditOutlined } from "@ant-design/icons";
 
 import axios from "axios";
@@ -9,6 +9,26 @@ const url = "https://warm-shore-85713.herokuapp.com/characters";
 
 const App = () => {
   const [dataAPI, setDataAPI] = useState([]);
+  const [modalInsertar, setModalInsertar] = useState(false);
+  const [heroeSeleccionado, setHeroeSeleccionado] = useState({
+    name: "",
+    game: "",
+    gender: "",
+    kind: "",
+    createdBy: "",
+    imageURL: "",
+    description: "",
+  });
+
+  const handleChange = (e) => {
+    const { id, value } = e.target;
+    setHeroeSeleccionado((prevState) => ({
+      ...prevState,
+      [id]: [value],
+    }));
+
+    console.log(heroeSeleccionado);
+  };
 
   const peticionGet = async () => {
     await axios.get(url).then((response) => {
@@ -21,8 +41,25 @@ const App = () => {
         };
       });
       setDataAPI(heroes);
-      console.log(heroes);
     });
+  };
+
+  const peticionPost = async () => {
+    await axios.post(url, heroeSeleccionado).then((response) => {
+      const resultado = response.data;
+
+      const heroeAux = {
+        ...resultado.data,
+        id: resultado.ref["@ref"].id,
+      };
+      console.log(resultado);
+      setDataAPI(dataAPI.concat(heroeAux));
+      abrirCerrarModalInsertar();
+    });
+  };
+
+  const abrirCerrarModalInsertar = () => {
+    setModalInsertar(!modalInsertar);
   };
 
   useEffect(async () => {
@@ -30,6 +67,11 @@ const App = () => {
   }, []);
   return (
     <div className="container">
+      <Button type="primary" onClick={abrirCerrarModalInsertar}>
+        Crear Héroe
+      </Button>
+      <br />
+      <br />
       <Row gutter={[16, 16]}>
         {dataAPI.map((item, index) => (
           <Col key={index} span={6}>
@@ -50,6 +92,48 @@ const App = () => {
           </Col>
         ))}
       </Row>
+
+      <Modal
+        centered
+        title="Crear Héroe"
+        onCancel={abrirCerrarModalInsertar}
+        onOk={peticionPost}
+        visible={modalInsertar}
+        cancelText="Cancelar"
+        okText="Crear"
+      >
+        <Input
+          id="name"
+          placeholder="Nombre del héreo"
+          onChange={handleChange}
+        />
+        <Input
+          id="game"
+          placeholder="Nombre del juego"
+          onChange={handleChange}
+        />
+        <Input
+          id="gender"
+          placeholder="Género del héroe"
+          onChange={handleChange}
+        />
+        <Input id="kind" placeholder="Clase de héroe" onChange={handleChange} />
+        <Input
+          id="createdBy"
+          placeholder="Creador del juego"
+          onChange={handleChange}
+        />
+        <Input
+          id="imageURL"
+          placeholder="URL correspondiente a la imágen del héreo"
+          onChange={handleChange}
+        />
+        <Input
+          id="description"
+          placeholder="Descripción del héreo"
+          onChange={handleChange}
+        />
+      </Modal>
     </div>
   );
 };
